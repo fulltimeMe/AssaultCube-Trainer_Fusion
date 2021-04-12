ff01379e40c12fcc1204746167ed86e0278c3dc9
fulltimeMe <68926695+fulltimeMe@users.noreply.github.com>
2021-04-12 08:58:21 +0200

    Add files via upload

---
+++ ac_externEspAutoAim/AssaultCubeHack/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing;
+using MYClasses;
+
+namespace AssaultCubeHack {
+
+    class Program {
+        const Int32 SW_MINIMIZE = 6;
+
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow([In] IntPtr hWnd, [In] Int32 nCmdShow);
+
+        private static void MinimizeConsoleWindow()
+        {
+            IntPtr hWndConsole = GetConsoleWindow();
+            ShowWindow(hWndConsole, 0);
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args) {
+            Console.Title = "ESP & AimClip Runtime";
+            Thread TH = new Thread(CheckForProc);
+            TH.Start();
+            MinimizeConsoleWindow();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AssaultHack());       
+        }
+
+        static void CheckForProc()
+        {
+            while (true)
+            {
+                Process[] processes = Process.GetProcessesByName("ac_client");
+                if (processes.Length == 0)
+                {
+                    Environment.Exit(-1);
+                }
+            }
+        }
+
+        public string hotkeyStrFromFile(string filePath)
+        {
+            string res = string.Empty;
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] sub = line.Split(':');
+                if (sub[0] == "AutoAimKey")
+                {
+                    res = sub[1].Replace(" ", string.Empty);
+                }
+            }
+            return res;
+        }
+
+    }
+
+    public class myStuff
+    {
+        public string name = "Fusion";
+        public string version = "0.0.1";
+        public Color playerTextColor = Color.FromArgb(66, 182, 245);
+        public Color ClientTextColor = Color.FromArgb(221, 66, 245);
+        public Font myFont = new Font("Unispace", 14, FontStyle.Bold);
+        public Keys myAimKey = new STK_Converter().GetKeyFromString(new Program().hotkeyStrFromFile("config/config.txt"));
+        public Color TeamC = Color.FromArgb(52, 216, 235);
+        public Color EnemyC = Color.FromArgb(162, 0, 255);
+    }
+}
---
+++ ac_externEspAutoAim/AssaultCubeHack/util/MYClasses.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System;
+
+namespace MYClasses
+{
+    class STK_Converter
+    {
+        public Keys[] keys = {Keys.Control, Keys.Alt, Keys.CapsLock, Keys.ShiftKey, Keys.Back,
+            Keys.Q, Keys.W, Keys.E, Keys.R, Keys.T, Keys.Z, Keys.U, Keys.I, Keys.O, Keys.P,
+            Keys.A, Keys.S, Keys.D, Keys.F, Keys.G, Keys.H, Keys.J, Keys.K, Keys.L, Keys.Multiply,
+            Keys.Divide, Keys.Shift, Keys.Add, Keys.Y, Keys.X, Keys.C, Keys.V, Keys.B, Keys.N,
+            Keys.M, Keys.NumPad0, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3, Keys.NumPad4,
+            Keys.NumPad5, Keys.NumPad6, Keys.NumPad7, Keys.NumPad8, Keys.NumPad9, Keys.ControlKey,
+            Keys.MButton, Keys.LButton, Keys.RButton, Keys.CapsLock};
+            
+        public Keys GetKeyFromString(string keyString)
+        {
+            Keys resKey = Keys.CapsLock;
+            
+            foreach (Keys key in keys)
+            {
+                string keyStr = key.ToString();
+                if (keyString == keyStr)
+                {
+                    resKey = key;
+                }
+            }
+            return resKey;
+        }
+    }
+}
